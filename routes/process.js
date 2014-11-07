532c73e4d4305aff532405c3b7b84e2aca02590d
wang-q <wang-q@outlook.com>
2014-11-07 10:03:22 +0800

    View job arguments

--- routes/process.js
+++ routes/process.js
@@ -4,8 +4,20 @@ var Job = require('../models/Job');
 
 router.get('/', function (req, res, next) {
     var username = req.user.username;
-
     Job.findOne({"username": username, status: "running"}).exec(function (error, job) {
+        if (error) return next(error);
+        res.render('process', {
+            title: 'EGA Process',
+            user:  req.user,
+            id:    'process',
+            job:   job ? JSON.stringify(job) : ''
+        });
+    });
+});
+
+router.get('/:name', function (req, res, next) {
+    var username = req.user.username;
+    Job.findOne({"username": username, "name": req.params.name}).exec(function (error, job) {
         if (error) return next(error);
         res.render('process', {
             title: 'EGA Process',
