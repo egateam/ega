f9179a619f364b176cac95b5fbb2ee0cd3e98ce3
wang-q <wang-q@outlook.com>
2015-01-10 09:03:45 +0800

    Add "Size" column to file list

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -143,3 +143,18 @@ egaApp.controller("ProcessShCtrl",
             });
         };
     });
+
+// http://gist.github.com/thomseddon/3511330
+egaApp.filter('bytes', function() {
+    return function(bytes, precision) {
+        if (bytes === 0) { return '0 bytes' }
+        if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+        if (typeof precision === 'undefined') precision = 1;
+
+        var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+            number = Math.floor(Math.log(bytes) / Math.log(1024)),
+            val = (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision);
+
+        return  (val.match(/\.0*$/) ? val.substr(0, val.indexOf('.')) : val) +  ' ' + units[number];
+    }
+});
