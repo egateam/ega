f5ad50727a9a7daf8bfa99b0c6c84716731c9a98
wang-q <wang-q@outlook.com>
2016-01-09 23:58:56 +0800

    Misc changes

--- public/downloads/download.sh
+++ public/downloads/download.sh
@@ -2,13 +2,11 @@
 
 DOWNLOADDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cd ${DOWNLOADDIR}
+wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-Win.exe
+wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-OSX.dmg
+wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.12/Oracle_VM_VirtualBox_Extension_Pack-5.0.12-104815.vbox-extpack
 
-wget -N http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-Win.exe
-wget -N http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-OSX.dmg
-wget -N http://download.virtualbox.org/virtualbox/5.0.12/Oracle_VM_VirtualBox_Extension_Pack-5.0.12-104815.vbox-extpack
+wget -N -P ${DOWNLOADDIR} https://raw.githubusercontent.com/egateam/egavm/master/vm/Vagrantfile
 
-wget -N https://raw.githubusercontent.com/egateam/egavm/master/vm/Vagrantfile
-
-wget -N https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1.dmg
-wget -N https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1.msi
+wget -N -P ${DOWNLOADDIR} https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1.dmg
+wget -N -P ${DOWNLOADDIR} https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1.msi
