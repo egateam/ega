d4be2f68ff065721a1073774d2d4274ba97d82c4
wang-q <wang-q@outlook.com>
2016-03-19 05:19:56 +0800

    Newer version

--- public/downloads/download.sh
+++ public/downloads/download.sh
@@ -2,9 +2,9 @@
 
 DOWNLOADDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-Win.exe
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-OSX.dmg
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.12/Oracle_VM_VirtualBox_Extension_Pack-5.0.12-104815.vbox-extpack
+wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.16/VirtualBox-5.0.16-105871-OSX.dmg
+wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.16/VirtualBox-5.0.16-105871-Win.exe
+wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.0.16/Oracle_VM_VirtualBox_Extension_Pack-5.0.16-105871.vbox-extpack
 
 wget -N -P ${DOWNLOADDIR} https://raw.githubusercontent.com/egateam/egavm/master/vm/Vagrantfile
 
